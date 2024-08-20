ae05fcab96b3b669943dc253ef479b689f1bb7c4
furioncycle <ttecho2021@gmail.com>
2024-08-19 21:34:28 -0700

    about page updated

--- build.zig
+++ build.zig
@@ -11,7 +11,7 @@ pub fn build(b: *std.Build) void {
     zine.website(b, .{
         .layouts_dir_path = "layouts",
         .content_dir_path = "content",
-        .static_dir_path = "static",
+        .assets_dir_path = "assets",
         .title = "Sync'd Gaze",
         .host_url = "https://deaf.audio",
     });
