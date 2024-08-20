fd394f3dc199613707de415d5f8a478f02640091
furioncycle <ttecho2021@gmail.com>
2024-08-19 22:45:28 -0700

    Updated to zine assets specification and removed extra fonts

--- build.zig
+++ build.zig
@@ -14,5 +14,20 @@ pub fn build(b: *std.Build) void {
         .assets_dir_path = "assets",
         .title = "Sync'd Gaze",
         .host_url = "https://deaf.audio",
+        .static_assets = &.{
+            "fonts/AtkinsonHyperlegible/web/WOFF/Atkinson-Hyperlegible-Bold-102.woff",
+            "fonts/AtkinsonHyperlegible/web/WOFF2/Atkinson-Hyperlegible-Bold-102a.woff2",
+            "fonts/AtkinsonHyperlegible/web/WOFF/Atkinson-Hyperlegible-BoldItalic-102.woff",
+            "fonts/AtkinsonHyperlegible/web/WOFF2/Atkinson-Hyperlegible-BoldItalic-102a.woff2",
+            "fonts/AtkinsonHyperlegible/web/WOFF/Atkinson-Hyperlegible-Italic-102.woff",
+            "fonts/AtkinsonHyperlegible/web/WOFF2/Atkinson-Hyperlegible-Italic-102a.woff2",
+            "fonts/AtkinsonHyperlegible/web/WOFF/Atkinson-Hyperlegible-Regular-102.woff",
+            "fonts/AtkinsonHyperlegible/web/WOFF2/Atkinson-Hyperlegible-Regular-102a.woff2",
+            "fonts/martianmono/MartianMono-StdRg.ttf",
+            "fonts/martianmono/MartianMono-StdBd.ttf",
+            "fonts/AtkinsonHyperlegible/AtkinsonMonolegible.ttf",
+            "zig.png",
+            "nixos-logo-only-hires.png",
+        },
     });
 }
